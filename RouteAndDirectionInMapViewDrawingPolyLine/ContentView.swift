75bca0bdd86a60206fe980b7749c5aa23d534502
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-11-10 15:35:06 +0200

    init

--- RouteAndDirectionInMapViewDrawingPolyLine/ContentView.swift
+++ RouteAndDirectionInMapViewDrawingPolyLine/ContentView.swift
@@ -7,15 +7,80 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        mapView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct mapView: UIViewRepresentable {
+    
+    func makeCoordinator() -> mapView.Coordinator {
+        return mapView.Coordinator()
+    }
+    
+    func makeUIView(context: UIViewRepresentableContext<mapView>) -> MKMapView {
+       
+        let map = MKMapView()
+        
+        let sourceCoordinate = CLLocationCoordinate2D(latitude: 13.086, longitude: 80.2707)
+        let destinationCoordinate = CLLocationCoordinate2D(latitude: 12.9830, longitude: 80.2594)
+        
+        let region = MKCoordinateRegion(center: sourceCoordinate, latitudinalMeters: 100000, longitudinalMeters: 100000)
+        
+        let sourcePin = MKPointAnnotation()
+        sourcePin.coordinate = sourceCoordinate
+        sourcePin.title = "Source"
+        map.addAnnotation(sourcePin)
+        
+        let destinationPin = MKPointAnnotation()
+        destinationPin.coordinate = destinationCoordinate
+        destinationPin.title = "Destination"
+        map.addAnnotation(destinationPin)
+        
+        map.region = region
+        map.delegate = context.coordinator
+        
+        let req = MKDirections.Request()
+        req.source = MKMapItem(placemark: MKPlacemark(coordinate: sourceCoordinate))
+        req.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate))
+        
+        let directions = MKDirections(request: req)
+        
+        directions.calculate { (direct, err) in
+            if err != nil {
+                print((err?.localizedDescription)!)
+                return
+            }
+            
+            let polyline = direct?.routes.first?.polyline
+            map.addOverlay(polyline!)
+            map.setRegion(MKCoordinateRegion(polyline!.boundingMapRect), animated: true)
+        }
+                
+        return map
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<mapView>) {
+        
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate {
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            
+            let render = MKPolylineRenderer(overlay: overlay)
+            render.strokeColor = .orange
+            render.lineWidth = 4
+            return render
+        }
     }
 }
